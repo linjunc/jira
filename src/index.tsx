d0c7481b8d18762b2c237b6a5fad39d9dd5a98ec
linjunc <ljc15113180751@163.com>
2021-09-20 22:02:37 +0800

    安装 devtool

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
+/*
+ * @Author: 林俊丞
+ * @Date: 2021-09-20 13:21:38
+ * @LastEditors: 林俊丞
+ * @LastEditTime: 2021-09-20 21:55:39
+ * @Description:  
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { loadDevTools } from 'jira-dev-tool'
 
-ReactDOM.render(
+loadDevTools(() => ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
