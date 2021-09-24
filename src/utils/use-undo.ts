8405d58156942cec2e2c889070efd4e15526c278
linjunc <ljc15113180751@163.com>
2021-09-24 22:14:16 +0800

    采用 reducer 方法改写use undo demo

--- src/utils/use-undo.ts
+++ src/utils/use-undo.ts
@@ -1,28 +1,27 @@
-import { useState, useCallback } from 'react';
-export const useUndo = <T>(initialPresent: T) => {
-    // // 保存历史记录
-    // const [past, setPast] = useState<T[]>([])
-    // const [present, setPresent] = useState(initialPresent)
-    // const [future, setFuture] = useState<T[]>([])
-    // 通过这种方式减少复杂度，太多的状态会难以维护
-    const [state, setState] = useState<{
-        past: T[],
-        present: T,
-        future: T[]
-    }>({
-        past: [],
-        present: initialPresent,
-        future: []
-    })
-    const canUndo = state.past.length !== 0
-    // 有future 才可以往前跳
-    const canRedo = state.future.length !== 0
-    // 回退
-    const undo = useCallback(() => {
-        // 采用函数形式，当前的 currentState 是当前的 state
-        setState(currentState => {
-            const { past, present, future } = currentState
-            if (past.length === 0) return currentState
+import { useCallback, useReducer } from 'react';
+// 定义 constant 定量
+const UNDO = 'UNDO'
+const REDO = 'REDO'
+const SET = 'SET'
+const RESET = 'RESET'
+
+// 定义一个常用的类型
+type State<T> = {
+    past: T[],
+    present: T,
+    future: T[]
+}
+type Action<T> = {
+    newPresent?: T,
+    type: typeof UNDO | typeof REDO | typeof SET | typeof RESET
+}
+// reducer
+const undoReducer = <T>(state: State<T>, action: Action<T>) => {
+    const { past, present, future } = state
+    const { type, newPresent } = action
+    switch (type) {
+        case UNDO: {
+            if (past.length === 0) return state
             // 找到最新的那个数据
             const previous = past[past.length - 1]
             // 取出最后一个
@@ -32,19 +31,9 @@ export const useUndo = <T>(initialPresent: T) => {
                 present: previous,
                 future: [present, ...future]
             }
-            // newPast中不包含最新的那个结果 回到过去一个
-            // setPast(newPast)
-            // // 前面的列表就少了一个 ，把previous的值给现在显示的值
-            // setPresent(previous)
-            // // 那未来就多了一个,最新的在最前面
-            // setFuture([previous, ...future])
-        })
-    }, [])
-    // 前进
-    const redo = useCallback(() => {
-        setState(currentState => {
-            const { past, present, future } = currentState
-            if (future.length === 0) return currentState
+        }
+        case REDO: {
+            if (future.length === 0) return state
             const next = future[0]
             // 取第一个
             const newFuture = future.slice(1)
@@ -53,34 +42,42 @@ export const useUndo = <T>(initialPresent: T) => {
                 present: next,
                 future: newFuture
             }
-        })
-
-    }, [])
-    // 加一
-    const set = useCallback((newPresent: T) => {
-        setState(currentState => {
-            const { past, present, future } = currentState
+        }
+        case SET: {
             if (newPresent === present) {
-                return currentState
+                return state
             }
             return {
                 past: [...past, present],
                 present: newPresent,
                 future: []
             }
-        })
-
-    }, [])
-    const reset = useCallback((newPresent: T) => {
-        setState(() => {
+        }
+        case RESET: {
             return {
                 past: [],
                 present: newPresent,
                 future: []
             }
-        })
-    }, [])
+        }
+    }
+    return state
+}
+export const useUndo = <T>(initialPresent: T) => {
+    const [state, dispatch] = useReducer(undoReducer, {
+        past: [],
+        present: initialPresent,
+        future: []
+    } as State<T>)
+    const canUndo = state.past.length !== 0
+    // 有future 才可以往前跳
+    const canRedo = state.future.length !== 0
     // 这里我们返回了函数，所以最好使用 useCallback
+    // 采用 reducer 进行状态管理
+    const undo = useCallback(() => dispatch({ type: UNDO }), [])
+    const redo = useCallback(() => dispatch({ type: REDO }), [])
+    const set = useCallback((newPresent: T) => dispatch({ type: SET, newPresent }), [])
+    const reset = useCallback((newPresent: T) => dispatch({ type: RESET, newPresent }), [])
     return [
         state,
         { set, reset, undo, redo, canUndo, canRedo }
