9e65db84a2d01c2e152171593ad0afaf432a0e38
linjunc <ljc15113180751@163.com>
2021-09-27 12:23:48 +0800

    优化看板列表

--- src/screens/kanban/kanban-columu.tsx
+++ src/screens/kanban/kanban-columu.tsx
@@ -5,6 +5,8 @@ import { useTaskTypes } from '../../utils/task-type';
 // 引入图片资源
 import taskIcon from 'assets/task.svg'
 import bugIcon from 'assets/bug.svg'
+import styled from '@emotion/styled';
+import { Card } from "antd";
 // 通过type渲染图片
 const TaskTypeIcon = ({ id }: { id: number }) => {
     const { data: taskTypes } = useTaskTypes()
@@ -12,19 +14,40 @@ const TaskTypeIcon = ({ id }: { id: number }) => {
     if (!name) {
         return null
     }
-    return <img src={name === 'task' ? taskIcon : bugIcon } />
+    return <img src={name === 'task' ? taskIcon : bugIcon} />
 }
-  
+
 export const KanbanColumn = ({ kanban }: { kanban: Kanban }) => {
     const { data: allTasks } = useTasks(useTasksSearchParams())
     const tasks = allTasks?.filter(task => task.kanbanId === kanban.id)
-    return <div>
+    return <Container>
         <h3>{kanban.name} </h3>
-        {
-            tasks?.map(tasks => <div key={tasks.id}>
-                {tasks.name}
-                <TaskTypeIcon id={tasks.typeId} />
-            </div>)
-        }
-    </div>
+        <TasksContainer>
+            {
+                tasks?.map(tasks => <Card style={{ marginBottom: '0.5rem' }} key={tasks.id}>
+                    <div>
+                        {tasks.name}
+                    </div>
+                    <TaskTypeIcon id={tasks.typeId} />
+                </Card>)
+            }
+        </TasksContainer>
+
+    </Container>
 }
+const Container = styled.div`
+    min-width: 27rem;
+    border-radius: 6px;
+    background-color: rgb(244,245,247);
+    display: flex;
+    flex-direction: column;
+    padding: 0.7rem 0.7rem 1rem;
+    margin-right:1.5rem;
+`
+const TasksContainer = styled.div`
+    overflow: scroll;
+    flex: 1;
+    ::-webkit-scrollbar {
+        display: none;
+    }
+`
